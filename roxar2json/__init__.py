2920417861ff5a9777650b0bd497309caabd4859
nilscb <Nils.Bonnevie@emerson.com>
2021-06-16 10:44:31 +0200

    fault_lines

--- roxar2json/__init__.py
+++ roxar2json/__init__.py
@@ -44,6 +44,21 @@ def get_wells_geojson(project):
         geometry.append(get_well_geojson(well))
     return geometry
 
+def get_fault_polygons(project):
+    print(horizon_name)
+    item = project.horizons['Top_Hugin']['ExtractedFaultLines']
+    poly_data = item.get_values()
+    
+    features = []
+    for line in poly_data:
+        polygon = geojson.create_polygon(line[:3,:2].tolist())
+
+        feature = geojson.create_feature(polygon, "Top_Hugin", [0, 0, 0, 255])
+        features.append(feature)
+
+    feature_collection = geojson.create_feature_collection(features)
+    return feature_collection
+
 def get_stratigraphy_json(project):
     stratigraphy = {'horizons': [], 'zones': []}
     zones = project.zones
